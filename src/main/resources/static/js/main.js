b4ace4284c63bb3b08b47c019997a56183c139a2
Felipe Granda R <felipegranda@hotmail.com>
2022-09-25 06:09:15 -0500

    added change event

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -215,7 +215,7 @@ const eventListenerUsersToDataList = event => {
 }
 
 movementsFormUsersListSelect.addEventListener('click', eventListenerUsersToDataList, false);
-
+movementsFormUsersListSelect.addEventListener('change', eventListenerUsersToDataList, false);
 
 
 
@@ -314,7 +314,7 @@ const eventListenerEmpresaToDataList = event =>{
 }
 
 usersFormEmpresaListSelect.addEventListener('click', eventListenerEmpresaToDataList,false)
-
+usersFormEmpresaListSelect.addEventListener('change', eventListenerEmpresaToDataList,false)
 
 
 
